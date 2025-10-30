a632e02b2ea38761546ab4544b1bfe35192aa73d
ompaiano <182190352+ompaiano@users.noreply.github.com>
2025-10-29 21:54:59 -0300

    added soliton pulse.

--- gnlse/solitons/soliton_nonlinear_pulse_propagation.py
+++ gnlse/solitons/soliton_nonlinear_pulse_propagation.py
@@ -4,6 +4,11 @@ from matplotlib import rcParams
 
 import gnlse
 
+
+def calc_power_for_soliton_sech(beta2, gamma, t0):
+    return -beta2/(gamma*t0**2)
+
+
 # Matplotlib Parameters:
 rcParams['font.family'] = 'serif'
 rcParams['font.size'] = '12'
@@ -24,20 +29,23 @@ rcParams['svg.fonttype'] = 'none'
 rcParams["figure.frameon"] = False
 
 
+
 # Pulse params:
 wavelength = 775 # [nm]
-power = 10 # [W]
+power = None # [W]
 tFWHM = 0.100 # Input pulse: pulse duration [ps]
 t0 = tFWHM / 2 / np.sqrt(np.log(2))  # for dispersive length calculations
 
 # Optical mode params:
     # Dispersion: derivatives of propagation constant at central wavelength
     # n derivatives of betas are in [ps^n/m]
-beta2 = -10.4e-3
-beta3 = -0.263e-3 # -0.263e-3
+beta2 = -10.371877e-3
+beta3 = 0.0 # -0.262858e-3
 betas = np.array([beta2, beta3])
+gamma = 0.47715253369 # Nonlinear coefficient [1/W/m]
 
-
+power = calc_power_for_soliton_sech(beta2*1e-24, gamma, 1.0*tFWHM*1e-12)
+print("Required power for soliton: ", power, " W.")
 
 setup = gnlse.gnlse.GNLSESetup()
 # Numerical parameters
@@ -59,16 +67,17 @@ setup.atol = 1e-6
 # Central wavelength [nm]
 setup.wavelength = wavelength
 # Nonlinear coefficient [1/W/m]
-setup.nonlinearity = 0.0
+setup.nonlinearity = gamma
 
 ###########################################################################
 # Dispersive length
 LD = t0 ** 2 / np.abs(betas[0])
 # Input pulse: peak power [W]
 # Fiber length [m]
-setup.fiber_length = 1
+setup.fiber_length = 100
 # Type of pulse:  gaussian
-setup.pulse_model = gnlse.GaussianEnvelope(power, tFWHM)
+m0 = 2 * np.log(1 + np.sqrt(2)) # this factor is because of the internal definition the SechEnvelope function
+setup.pulse_model = gnlse.SechEnvelope(power, tFWHM*m0)
 # Loss coefficient [dB/m]
 loss = 0
 # Type of dyspersion operator: build from Taylor expansion
@@ -87,7 +96,7 @@ sol = solver.run()
 ###########################################################################
 
 fig, (ax1, ax2, ax3) = plt.subplots(figsize=(5, 15), nrows=3)
-_, im1 = gnlse.plot_wavelength_vs_distance(sol, WL_range=[700, 850], ax=ax1, cmap="gnuplot")
+_, im1 = gnlse.plot_wavelength_vs_distance(sol, WL_range=[750, 800], ax=ax1, cmap="gnuplot")
 # ax1.text(-0.0, 0.05, 'a)')
 ax1.set_title("a)" + " "*65)
 cbar = fig.colorbar(im1, ax=ax1, location='right', anchor=(0, 0.3))
@@ -98,34 +107,39 @@ ax2.set_title("b)" + " "*65)
 cbar = fig.colorbar(im2, ax=ax2, location='right', anchor=(0, 0.3))
 cbar.set_label('Phase [degrees]')
 
-_, im3 = gnlse.plot_delay_vs_distance(sol, time_range=[-2.0, 2.0], ax=ax3, cmap="gnuplot")
-ax3.set_title("c)" + " "*65)
+_, im3 = gnlse.plot_delay_vs_distance(sol, time_range=[-1.0, 1.0], ax=ax3, cmap="gnuplot")
+ax3.set_title("b)" + " "*65)
 cbar = fig.colorbar(im3, ax=ax3, location='right', anchor=(0, 0.3))
 cbar.set_label('Normalized Power Density')
 
-plt.savefig("linear_pulse_b3=-0.263e-3.png", dpi=200, bbox_inches="tight")
+plt.savefig(f"soliton_nonlinear_pulse_gamma{gamma}_b3={beta3}.png", dpi=200, bbox_inches="tight")
 plt.show()
-
-
+plt.close()
 
 plt.plot(sol.t,np.abs(sol.At[0,:]) / np.max(np.abs(sol.At[0,:])), label="z = 0 m")
 # plt.plot(sol.t,np.real(sol.At[-1,:]))
-plt.plot(sol.t,np.abs(sol.At[-1,:]) / np.max(np.abs(sol.At[-1,:])),'-.', label="z = 1 m; $\\beta_3 = -0.263$ ps³/km")
+plt.plot(sol.t,np.abs(sol.At[-1,:]) / np.max(np.abs(sol.At[-1,:])),'-.', label="z = 100 m; Peak-Power = $P_0$")
 
 
-# Type of dyspersion operator: build from Taylor expansion
-betas = np.array([betas[0], 0])
-setup.dispersion_model = gnlse.DispersionFiberFromTaylor(loss, betas)
+setup.pulse_model = gnlse.SechEnvelope(2*power, tFWHM*m0)
 solver = gnlse.gnlse.GNLSE(setup)
 sol = solver.run()
 plt.plot(
     sol.t,
     np.abs(sol.At[-1,:]) / np.max(np.abs(sol.At[-1,:])),
-    'g--', label="z = 1 m; $\\beta_3 = 0.0 $ ps³/km")
+    'g--', label="z = 100 m; Peak-Power = $2P_0$")
+
+setup.pulse_model = gnlse.SechEnvelope(power/2, tFWHM*m0)
+solver = gnlse.gnlse.GNLSE(setup)
+sol = solver.run()
+plt.plot(
+    sol.t,
+    np.abs(sol.At[-1,:]) / np.max(np.abs(sol.At[-1,:])),
+    'm:', label="z = 100 m; Peak-Power = $P_0/2$")
 
 plt.xlabel("Time (ps)")
 plt.ylabel("Normalized Envelope")
 plt.xlim([-0.5,0.5])
 plt.legend(loc="lower left",  prop={'size': 11})
-plt.savefig("linear_broadening.pdf")
+plt.savefig("soliton.pdf")
 plt.show()
